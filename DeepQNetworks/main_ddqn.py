e4e6cd2b28a6d9882e4319f94258eea67a8247ae
Srinath Jukanti <avior@Srinaths-MacBook-Pro.local>
2020-02-23 15:58:16 -0500

    double dqn agent

--- DeepQNetworks/main_ddqn.py
+++ DeepQNetworks/main_ddqn.py
@@ -30,8 +30,15 @@ if __name__ == '__main__':
                      replace_target_count=1000, epsilon_decay=1e-5,
                      checkpoint_dir=checkpoint_directory, algo='DDQNAgent',
                      env_name='PongNoFrameskip-v4')
+    
     if args.test:
-        agent.load_models()
+        try:
+            agent.load_models()
+        except:
+            print(f'Failed to load agent model \n \
+                   Please make sure model exists in {checkpoint_directory}')
+        finally:
+            env.close()
 
     filename = agent.algo + '_' + agent.env_name \
                 + '_lr' + str(agent.lr) + '_' + str(n_games) \
