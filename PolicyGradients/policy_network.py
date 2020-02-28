c0a4f4483ff596d3a74ef5fd78f9b0743fe99a9e
Srinath Jukanti <avior@Srinaths-MacBook-Pro.local>
2020-02-28 13:36:24 -0500

    utils

--- PolicyGradients/policy_network.py
+++ PolicyGradients/policy_network.py
@@ -11,20 +11,37 @@ class ReinforcePolicyNet(nn.Module):
         self.checkpoint_dir = checkpoint_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
 
-        self.fc1 = nn.Linear(input_dims[0], 128)
-        self.fc2 = nn.Linear(128, 256)
-        self.fc3 = nn.Linear(256, n_actions)
+        self.conv1 = nn.Conv2d(input_dims[0], 32, 8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
 
-        self.optimizer = optim.RMSprop(self.parameters(), lr=lr)
+        fc_input_dims = self.calculate_conv_output_dims(input_dims)
+
+        self.fc1 = nn.Linear(fc_input_dims, 512)
+        self.fc2 = nn.Linear(512, n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
     def forward(self, state):
-        hidden_state = F.relu(self.fc1(state))
-        hidden_state = F.relu(self.fc2(hidden_state))
-        actions = F.relu(self.fc3(hidden_state))
+        hidden_state = F.relu(self.conv1(state))
+        hidden_state = F.relu(self.conv2(hidden_state))
+        hidden_state = F.relu(self.conv3(hidden_state))
+
+        fc_input = hidden_state.view(hidden_state.size()[0], -1)
+        hidden_state = F.relu(self.fc1(fc_input))
+        actions = self.fc2(hidden_state)
 
         return F.softmax(actions)
+    
+    def calculate_conv_output_dims(self, input_dims):
+        state = T.zeros(1, *input_dims)
+        hidden_state = self.conv1(state)
+        hidden_state = self.conv2(hidden_state)
+        hidden_state = self.conv3(hidden_state)
+
+        return int(np.prod(hidden_state.size()))
 
     def save_checkpoint(self):
         print('##.....Saving checkpoint.....##')
